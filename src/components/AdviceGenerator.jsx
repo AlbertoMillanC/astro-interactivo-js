dafa198b507aac2f59fc7dcb58a6011cbb3f8b6a
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-01-13 00:04:55 -0500

    add styles tailwind

--- src/components/AdviceGenerator.jsx
+++ src/components/AdviceGenerator.jsx
@@ -12,7 +12,7 @@ const AdviceGenerator = ({advice}) => {
 } 
     return (
         <div className="text-center ">
-            <label className=" bg-blue-300 container text-center row-span-4 text-xl " >{response}</label>
+            <label className=" font-semibold mb-4 bg-gray-200 p-4 rounded-md " >"{response}"</label>
             <button className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full" onClick={() => onClickButton()}>click button</button>
             
         </div>
